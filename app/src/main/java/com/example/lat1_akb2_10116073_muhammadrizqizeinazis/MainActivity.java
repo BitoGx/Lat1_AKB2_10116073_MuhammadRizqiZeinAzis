c3770387898aa7f3ea728872fa805da00a1d711a
BitoGx <RizqiGx@gmail.com>
2019-04-04 15:04:02 +0700

    Backup

--- app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/MainActivity.java
+++ app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                startActivity(new Intent(MainActivity.this, verifikasi.class ));
+                startActivity(new Intent(MainActivity.this, Verifikasi.class ));
             }
         });
     }
--- app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/Verifikasi.java
+++ app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/Verifikasi.java
@@ -3,7 +3,7 @@ package com.example.lat1_akb2_10116073_muhammadrizqizeinazis;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class verifikasi extends AppCompatActivity
+public class Verifikasi extends AppCompatActivity
 {
   
   @Override
