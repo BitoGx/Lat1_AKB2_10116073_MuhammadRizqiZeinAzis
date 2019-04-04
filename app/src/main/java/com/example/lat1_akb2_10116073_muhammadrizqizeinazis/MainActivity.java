8484c80040d4fb0edde21975c044e71618a9d813
BitoGx <RizqiGx@gmail.com>
2019-04-04 14:46:19 +0700

    Activity 2 Verifikasi Kode

--- app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/MainActivity.java
+++ app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lat1_akb2_10116073_muhammadrizqizeinazis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  *Created By
@@ -20,6 +23,23 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome_page);
+        
+        ConfigureNextButton();
+        
     }
+    
+    private void ConfigureNextButton()
+    {
+        Button btn1 = (Button) findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, verifikasi.class ));
+            }
+        });
+    }
+    
 }
