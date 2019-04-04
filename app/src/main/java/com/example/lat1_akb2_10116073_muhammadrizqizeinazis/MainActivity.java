5d759486f4de7a01212bada92f327fba41d79eeb
BitoGx <RizqiGx@gmail.com>
2019-04-04 10:08:27 +0700

    Test

---
+++ app/src/main/java/com/example/lat1_akb2_10116073_muhammadrizqizeinazis/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lat1_akb2_10116073_muhammadrizqizeinazis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *Created By
+ *NIM   : 10116073
+ *Nama  : Muhammad Rizqi Zein Azis
+ *Kelas : IF-2 / AKB-2
+ *CHANGELOG April 3, 2019
+ * -  Start New Project
+ *CHANGELOG April 4, 2019
+ * - New Layout and Function
+ * test
+ */
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
